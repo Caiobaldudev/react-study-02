86f7ddc94798b9e9e66f69440764845262fda29f
Caio Balduino <caiobalduinodev@hotmail.com>
2024-08-06 17:02:11 -0300

    useState fixing exercise

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Exercicio from './Exercicios/Exercicio.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Exercicio />
   </React.StrictMode>,
 )
