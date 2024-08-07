224652ef93e8dcedae2e7f5044d8c5684940ca13
Caio Balduino <caiobalduinodev@hotmail.com>
2024-08-07 18:32:52 -0300

    Learning about useEffect_2 localStorage

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Exercicio from './Exercicios/Exercicio.jsx'
 import UEffect from './useEffect/UEffect.jsx'
+import ExercicioTwo from './ExercicioTwo/ExercicioTwo.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <UEffect />
+    <ExercicioTwo />
   </React.StrictMode>,
 )
